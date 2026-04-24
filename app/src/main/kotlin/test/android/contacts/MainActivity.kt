4e1d42fb0b74011917a109712fd69d94c2a89d80
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-24 15:18:17 +0300

    wip

--- app/src/main/kotlin/test/android/contacts/MainActivity.kt
+++ app/src/main/kotlin/test/android/contacts/MainActivity.kt
@@ -1,11 +1,36 @@
 package test.android.contacts
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 
 internal class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // todo
+        val context: Context = this
+        LinearLayout(context).also { root ->
+            root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+            )
+            root.orientation = LinearLayout.VERTICAL
+            root.gravity = Gravity.CENTER_VERTICAL
+            Button(context).also { view ->
+                view.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
+                view.text = "add call log"
+                view.setOnClickListener { _ ->
+                    // todo
+                }
+                root.addView(view)
+            }
+            setContentView(root)
+        }
     }
 }
